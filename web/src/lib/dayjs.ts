2aa83d1483643f59e9241d068bceda4fc778084d
julyanesbrito <168753813+julyanesbrito@users.noreply.github.com>
2025-07-12 16:16:57 -0300

    Criação layout com React e rota de criação de sala

---
+++ web/src/lib/dayjs.ts
@@ -0,0 +1,8 @@
+import dayJsLib from "dayjs";
+import 'dayjs/locale/pt-BR';
+import relativeTime from 'dayjs/plugin/relativeTime';
+
+dayJsLib.locale('pt-BR')
+dayJsLib.extend(relativeTime)
+
+export const dayjs = dayJsLib;
